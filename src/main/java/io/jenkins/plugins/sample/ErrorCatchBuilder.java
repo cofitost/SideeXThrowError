34d4fa2f4bb1c38ac627b4f8ae86770c15969bdd
cofitost <zxcv091922@gmail.com>
2020-05-29 04:13:29 +0800

    init

---
+++ src/main/java/io/jenkins/plugins/sample/ErrorCatchBuilder.java
@@ -0,0 +1,132 @@
+package io.jenkins.plugins.sample;
+
+import hudson.Launcher;
+import hudson.Extension;
+import hudson.FilePath;
+import hudson.model.AbstractProject;
+import hudson.model.Run;
+import hudson.model.TaskListener;
+import hudson.tasks.Builder;
+import hudson.tasks.BuildStepDescriptor;
+import org.kohsuke.stapler.DataBoundConstructor;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Base64;
+
+import jenkins.tasks.SimpleBuildStep;
+import org.jenkinsci.Symbol;
+import org.kohsuke.stapler.DataBoundSetter;
+
+public class ErrorCatchBuilder extends Builder implements SimpleBuildStep {
+
+    private final String jenkinsIp;
+
+    @DataBoundConstructor
+    public ErrorCatchBuilder(String jenkinsIp) {
+        this.jenkinsIp = jenkinsIp;
+    }
+
+    public String getSideeXConsoleText() {
+        // curl http://140.134.26.76:38200/job/W1/lastBuild/consoleText | Select-Object -Expand Content
+        String consoleUrl = "http://140.134.26.76:28888/job/m0720881_SideeX/lastBuild/consoleText";
+        HttpURLConnection conn = null;
+        StringBuilder contentBuilder = new StringBuilder();
+
+        try {
+          URL url = new URL(consoleUrl);
+          conn = (HttpURLConnection) url.openConnection();
+          String input = "root" + ":" + "selab245";
+          Base64.Encoder encoder = Base64.getEncoder();
+          String encoding = "Basic " + encoder.encodeToString(input.getBytes());
+          conn.setRequestProperty("Authorization", encoding);
+          conn.setReadTimeout(10000);
+          conn.setConnectTimeout(15000);
+          conn.setRequestMethod("GET");
+          conn.connect();
+          try (BufferedReader br = new BufferedReader(
+              new InputStreamReader(conn.getInputStream(), "UTF-8"));) {
+            String sCurrentLine;
+            while ((sCurrentLine = br.readLine()) != null) {
+                contentBuilder.append(sCurrentLine).append("\n");
+            }
+          } catch (IOException e) {
+            e.printStackTrace();
+          }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+          if (conn != null) {
+            conn.disconnect();
+          }
+        }
+
+        String SideeXConsole = contentBuilder.toString();
+        SideeXConsole = SideeXConsole.substring(SideeXConsole.indexOf("INFO Executing"), SideeXConsole.indexOf("SideeX Runner finished."));
+        return SideeXConsole;
+    }
+
+    public ArrayList<String> getCatchResult(String SideeXConsole) {
+        ArrayList<String> resultArray = new ArrayList<>();
+        String resultConsole = "";
+
+        while(SideeXConsole.contains("CATCH")) {
+            SideeXConsole = SideeXConsole.substring(SideeXConsole.indexOf("CATCH") + 5);
+            resultConsole = SideeXConsole.substring(SideeXConsole.indexOf("echo") + 7);
+            resultArray.add(resultConsole.substring(0, resultConsole.indexOf("|") - 1));
+        }
+
+        return resultArray;
+    }
+
+    public ArrayList<String> getErrorResult(String SideeXConsole) {
+        ArrayList<String> resultArray = new ArrayList<>();
+
+        while(SideeXConsole.contains("ERROR")) {
+            SideeXConsole = SideeXConsole.substring(SideeXConsole.indexOf("ERROR") + 6);
+            resultArray.add(SideeXConsole.substring(0, SideeXConsole.indexOf("\n")));
+        }
+
+        return resultArray;
+    }
+
+    @Override
+    public void perform(Run<?, ?> run, FilePath workspace, Launcher launcher, TaskListener listener) throws InterruptedException, IOException {
+        listener.getLogger().println("Start catching...");
+        String SideeXConsole = getSideeXConsoleText();
+        if(SideeXConsole.contains("CATCH") || SideeXConsole.contains("ERROR")) {
+          listener.getLogger().println("SideeX test failed with the reason below:");
+          listener.getLogger().println("");
+          for(String s : getCatchResult(SideeXConsole)) {
+            listener.getLogger().println(s);
+          }
+    
+          for(String s : getErrorResult(SideeXConsole)) {
+            listener.getLogger().println(s);
+          }
+          listener.getLogger().println("");
+          throw new IOException("SideeX Test Error");
+        }
+    }
+
+    @Symbol("greet")
+    @Extension
+    public static final class DescriptorImpl extends BuildStepDescriptor<Builder> {
+
+        @Override
+        public boolean isApplicable(Class<? extends AbstractProject> aClass) {
+            return true;
+        }
+
+        @Override
+        public String getDisplayName() {
+            return "SideeX Error Catcher";
+        }
+
+    }
+
+}
---
+++ src/test/java/io/jenkins/plugins/sample/ErrorCatchBuilderTest.java
@@ -0,0 +1,91 @@
+package io.jenkins.plugins.sample;
+
+import hudson.model.FreeStyleBuild;
+import hudson.model.FreeStyleProject;
+import hudson.model.Label;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition;
+import org.jenkinsci.plugins.workflow.job.WorkflowJob;
+import org.jenkinsci.plugins.workflow.job.WorkflowRun;
+import org.junit.Rule;
+import org.junit.Test;
+import org.jvnet.hudson.test.JenkinsRule;
+
+public class ErrorCatchBuilderTest {
+    // @Test
+    // public void test() {
+    //     String jenkinsIp = "";
+    //     ErrorCatchBuilder errorCatchBuilder = new ErrorCatchBuilder(jenkinsIp);
+    //     System.out.print(errorCatchBuilder.getCurrentConsoleText());
+    // }
+
+    public static void main(String[] args) {
+    //   ErrorCatchBuilderTest main = new ErrorCatchBuilderTest();
+    //   for(String s : main.getCatchResult(main.Builder("consoleText.txt"))){
+    //       System.out.println(s);
+    //   }
+
+    //   for(String s : main.getErrorResult(main.Builder("consoleText.txt"))){
+    //     System.out.println(s);
+    //   }
+    ErrorCatchBuilder errorCatchBuilder = new ErrorCatchBuilder("jenkinsIp");
+    String s = errorCatchBuilder.getSideeXConsoleText();
+    for(String s2 : errorCatchBuilder.getCatchResult(s)) {
+        System.out.println(s2);
+    }
+
+    for(String s2 : errorCatchBuilder.getErrorResult(s)) {
+        System.out.println(s2);
+    }
+    }
+
+    public String Builder(String filePath) {
+      StringBuilder contentBuilder = new StringBuilder();
+      try (BufferedReader br = new BufferedReader(new FileReader(filePath))) 
+      {
+        String sCurrentLine;
+        while ((sCurrentLine = br.readLine()) != null) 
+        {
+            contentBuilder.append(sCurrentLine).append("\n");
+        }
+      }  
+      catch (IOException e) 
+      {
+        e.printStackTrace();
+      }
+
+      String SideeXConsole = contentBuilder.toString();
+      SideeXConsole = SideeXConsole.substring(SideeXConsole.indexOf("INFO Executing"), SideeXConsole.indexOf("SideeX Runner finished."));
+      return SideeXConsole;
+    }
+
+    public ArrayList<String> getCatchResult(String SideeXConsole) {
+        ArrayList<String> resultArray = new ArrayList<>();
+        String resultConsole = "";
+
+        while(SideeXConsole.contains("CATCH")) {
+            SideeXConsole = SideeXConsole.substring(SideeXConsole.indexOf("CATCH") + 5);
+            resultConsole = SideeXConsole.substring(SideeXConsole.indexOf("echo") + 7);
+            resultArray.add(resultConsole.substring(0, resultConsole.indexOf("|") - 1));
+        }
+
+        return resultArray;
+    }
+
+    public ArrayList<String> getErrorResult(String SideeXConsole) {
+        ArrayList<String> resultArray = new ArrayList<>();
+
+        while(SideeXConsole.contains("ERROR")) {
+            SideeXConsole = SideeXConsole.substring(SideeXConsole.indexOf("ERROR") + 6);
+            resultArray.add(SideeXConsole.substring(0, SideeXConsole.indexOf("\n")));
+        }
+
+        return resultArray;
+    }
+
+}
